773db3e8ce2c2d0a3341c76450ab061bc302fb73
ЖилкибаевА <@403_2_>
2018-01-09 17:40:05 +0600

    gh

--- AdoNetModule01/MainWindow.xaml.cs
+++ AdoNetModule01/MainWindow.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+// для работы с App.congin/web.config
+using System.Configuration;
+
+using System.Data.OleDb;
+using System.Data.SqlClient;
 
 namespace AdoNetModule01
 {
@@ -20,9 +25,78 @@ namespace AdoNetModule01
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string conectonString = "";
         public MainWindow()
         {
             InitializeComponent();
+            //conectonString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            conectonString = ConfigurationManager.ConnectionStrings["OleDbConnection"].ConnectionString;
         }
+
+        private void ConnetToServerButton_Click(object sender, RoutedEventArgs e)
+        {
+            #region exml1
+            SqlConnection connection = new SqlConnection(conectonString);
+            try
+            {
+                connection.Open();
+                ConnectionMesseges.Content += "Подключение открыто ...\n";
+
+            }
+            catch(Exception ex)
+            {
+                ConnectionMesseges.Content += ex.Message + "\n";
+            }
+
+            finally
+            {
+                connection.Close();
+                ConnectionMesseges.Content += " Подключение закрыто ...\n ";
+            }
+            #endregion
+
+            #region exml2
+            using (SqlConnection conn = new SqlConnection(conectonString))
+            {
+                conn.Open();
+                ConnectionMesseges.Content += "Подключение открыто";
+                //получаем информацию о подключении
+                ConnectionMesseges.Content += "Свойство подключение\n";
+                ConnectionMesseges.Content += "\t --> строка " + conn.ConnectionString+ "\n";
+                ConnectionMesseges.Content += "\t --> база " + conn.Database + "\n";
+                ConnectionMesseges.Content += "\t --> сервер " + conn.ServerVersion + "\n";
+                ConnectionMesseges.Content += "\t --> стостояние " + conn.State + "\n";
+                ConnectionMesseges.Content += "\t --> рабочая станция " + conn.WorkstationId + "\n";
+            }
+            ConnectionMesseges.Content += " Подключение закрыто ...\n ";
+            #endregion
+
+            #region exml3
+
+            //ConnectionMesseges.Content = "";
+
+            //using (OleDbConnection conn = new OleDbConnection(conectonString))
+            //{
+            //    conn.Open();
+            //    ConnectionMesseges.Content += "Подключение открыто";
+            //    //получаем информацию о подключении
+            //    ConnectionMesseges.Content += "Свойство подключение\n";
+            //    ConnectionMesseges.Content += "\t --> строка " + conn.ConnectionString + "\n";
+            //    ConnectionMesseges.Content += "\t --> база " + conn.Database + "\n";
+            //    ConnectionMesseges.Content += "\t --> сервер " + conn.ServerVersion + "\n";
+            //    ConnectionMesseges.Content += "\t --> стостояние " + conn.State + "\n";
+                
+
+            //}
+            //ConnectionMesseges.Content += " Подключение закрыто ...\n ";
+            #endregion
+
+        }
+    }
+    
+    public class equiment
+    {
+        public int EquipmentId { get; set; }
+        public string GarageRoom { get; set; }
     }
 }
